6319b0d2aad7e048608ac3c1f45133fa0958f7e8
Bryan <bryanchang1203@hotmail.com>
2016-01-09 12:34:41 +0800

    Update main.js

--- main.js
+++ main.js
@@ -11,32 +11,34 @@ var enemy = {
     direction:{x:0,y:-1},
     speed:64,
     pathDes: 0,
-    move: function(){
-        this.x += this.direction.x * this.speed/FPS;
-        this.y += this.direction.y * this.speed/FPS;
+this.move = function(){
         if( isCollided(enemyPath[this.pathDes].x, enemyPath[this.pathDes].y, this.x, this.y, this.speed/FPS, this.speed/FPS) ){
 
-            // 首先，移動到下一個路徑點
-            this.x = enemyPath[this.pathDes].x;
-            this.y = enemyPath[this.pathDes].y;
+            if (this.pathDes === enemyPath.length-1) {
+                this.hp=0;
+                hp -= 10;
+            } else {
+                // 首先，移動到下一個路徑點
+                this.x = enemyPath[this.pathDes].x;
+                this.y = enemyPath[this.pathDes].y;
 
-            // 指定下一個路徑點
-            this.pathDes++;
+                // 指定下一個路徑點
+                this.pathDes++;
 
-            // 取得前往下一個路徑點的單位向量
-            var unitVector = getUnitVector( this.x, this.y, enemyPath[this.pathDes].x, enemyPath[this.pathDes].y );
-            this.direction.x = unitVector.x;
-            this.direction.y = unitVector.y;
-            
+                // 取得前往下一個路徑點的單位向量
+                var unitVector = getUnitVector( this.x, this.y, enemyPath[this.pathDes].x, enemyPath[this.pathDes].y );
+                this.direction.x = unitVector.x;
+                this.direction.y = unitVector.y;
+            }
+
         } else {
-             this.x += this.direction.x * this.speed/FPS;
+            // this.x += this.direction.x * this.speed/FPS;
             this.x = this.x + this.direction.x * this.speed/FPS;
-             this.y += this.direction.y * this.speed/FPS;
+            // this.y += this.direction.y * this.speed/FPS;
             this.y = this.y + this.direction.y * this.speed/FPS;
-
         }
-    }
-};
+    };
+}
 
 var enemyPath = [
     {x:96, y:352},
